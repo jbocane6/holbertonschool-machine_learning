8940986d31dff7e6eb7b5dcbcfee64b4883e26ff
Juan Bocanegra <juan.bocanegra@ci2.co>
2024-04-11 14:51:20 -0500

    [UPDATE] 2-size

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""Calculates the shape of a matrix"""
+
 
 def matrix_shape(matrix):
+    """
+    matrix_shape: validates that array is not null and get the shape
+
+    Returns: The shape should be returned as a list of integers
+    """
     if not isinstance(matrix, list):
         return []
     return [len(matrix)] + matrix_shape(matrix[0])
---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""transpose a 2D matrix"""
+
+
+def matrix_transpose(matrix):
+    """
+    matrix_transpose: returns the transpose of a 2D matrix
+    
+    Returns: the transpose of a 2D matrix
+    """
+    for index in len(matrix[0]):
+        for value in index:
+            print(value)
