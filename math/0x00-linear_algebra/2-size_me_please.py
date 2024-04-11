881af577932108eec9e955ad299160bcb4126690
Juan Camilo Bocanegra Osorio <75183250+jbocane6@users.noreply.github.com>
2024-04-11 13:53:00 -0500

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """Calculates the shape of a matrix"""
 
-
 def matrix_shape(matrix):
     """
     matrix_shape: validates that array is not null and get the shape
 
     Returns: The shape should be returned as a list of integers
     """
-    if type(matrix) is list:
-        return [len(matrix), *matrix_shape(matrix[0])]
-    else:
+    if not isinstance(matrix, list):
         return []
+    return [len(matrix)] + matrix_shape(matrix[0])
